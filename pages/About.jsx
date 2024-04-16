eced18655626c1bc87ad7c756cb3e4d59811c3c5
Annak506 <annakleinloog@hotmail.com>
2024-04-16 13:01:38 +0300

    toch contact gegevens links gecentreerd

--- pages/About.jsx
+++ pages/About.jsx
@@ -47,13 +47,11 @@ export default function About() {
             krachtig middel zijn om emoties te uiten, trauma’s te verwerken en
             een nieuw perspectief op het leven te creëren.
           </p>
-          <div class="contact-info">
-            <p>
-              <strong>Contactgegevens </strong>
-            <br></br>
-            E-mail: logos@kleinloog.eu<br></br>
-            [◉°] schrijvenencoaching</p>
-         </div>
+          <p>
+            <strong>Contactgegevens </strong>
+          <br></br>
+          E-mail: logos@kleinloog.eu<br></br>
+          [◉°] schrijvenencoaching</p>
         </div>
         <div className="about-page-cta">
           <h2>

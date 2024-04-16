098db68f48485e665823cdaac24ad08c1698bcd4
Annak506 <annakleinloog@hotmail.com>
2024-04-16 12:59:01 +0300

    contact info gecentreerd

--- pages/About.jsx
+++ pages/About.jsx
@@ -47,11 +47,13 @@ export default function About() {
             krachtig middel zijn om emoties te uiten, trauma’s te verwerken en
             een nieuw perspectief op het leven te creëren.
           </p>
-          <p>
-            <strong>Contactgegevens </strong>
-          <br></br>
-          E-mail: logos@kleinloog.eu<br></br>
-          [◉°] schrijvenencoaching</p>
+          <div class="contact-info">
+            <p>
+              <strong>Contactgegevens </strong>
+            <br></br>
+            E-mail: logos@kleinloog.eu<br></br>
+            [◉°] schrijvenencoaching</p>
+          <div class="contact-info"></div>
         </div>
         <div className="about-page-cta">
           <h2>

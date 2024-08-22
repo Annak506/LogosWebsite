92fc3603dcf264f78d456b49324dc6611c7c630c
Annak506 <annakleinloog@hotmail.com>
2024-08-22 15:32:36 +0300

    blog pagina toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -8,16 +8,26 @@ import Reviews from './pages/Reviews';
 import './App.css';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <BrowserRouter>
       <header>
         <Link className="site-logo" to="/">
           <strong>#Logos</strong>
         </Link>
-        <nav>
+        <button className="menu-button" onClick={toggleMenu}>
+          ☰
+        </button>
+        <nav className={`menu ${isMenuOpen ? 'open' : ''}`}>
           <Link to="/about">Over ons</Link>
           <Link to="/information">Cursussen</Link>
-          <Link to ="/reviews">Recensies</Link>
+          <Link to="/reviews">Recensies</Link>
+          <Link to="https://logosblogs.netlify.app/">Blogs</Link>
         </nav>
       </header>
       <Routes>

60071d7330ccfc6f9a3fdec24398036938e8cd43
Annak506 <116018850+Annak506@users.noreply.github.com>
2025-11-16 10:47:02 +0100

    homepagina tekst aangepast

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
     <div className="home-container">
       <h1>Welkom bij Logos Schrijven en Coaching.</h1>
       <p>
-       Logos Schrijven & Coaching biedt ambachtelijke, individuele schrijflessen en schrijfcoaching op maat. Voor iedereen die zijn techniek wil aanscherpen, een project wil vormgeven of via schrijven verdieping zoekt.
+       Wij bieden ambachtelijke, individuele schrijflessen en schrijfcoaching op maat. Voor iedereen die zijn techniek wil aanscherpen, een project wil vormgeven of via schrijven verdieping zoekt.
       </p>
       <Link to="information" title="Ontdek onze diverse schrijfcursussen en coachingstrajecten, online en in Amsterdam">
         Vind jouw cursus

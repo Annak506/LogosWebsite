4d8810b3288a091b124b01fbcdc6ed8b5bad5a16
Annak506 <116018850+Annak506@users.noreply.github.com>
2026-02-12 15:23:53 +0100

    test page contact container

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contact() {
 
   return (
-    <div className="contact-container">
+    <div className="page contact-container">
       <h2>Contacteer ons hier</h2>
       <p>
         📧 E-mail:{" "}
